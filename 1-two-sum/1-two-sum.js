c7b616e549cb0ebca912525a7929f0d05c9c984b
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-15 04:24:56 +0500

    Time: 70 ms (89.58%), Space: 42.8 MB (40.97%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,22 +4,34 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // Brute-Force Solution, Not Efficient, Time Complexity(n*n)
+    // Brute-Force Solution, Not Efficient, Time Complexity = O(n*n)
     // for(let i=0; i< nums.length; i++) {
     //     for(let j=i+1; j<nums.length; j++) {
     //         if(nums[i]+ nums[j] == target)
     //             return [i,j];
     //     }
     // }
-    let values = new Map();
-    for(let i=0; i< nums.length; i++) {
-        let complement = target - nums[i];
-        if(values.has(complement)) {
-            return [values.get(complement), i];
-        }
-        values.set(nums[i],i);
-    }
-    return [];
+    // 2nd Solution, Time Complexity = O(n)
+    // let values = new Map();
+    // for(let i=0; i< nums.length; i++) {
+    //     let complement = target - nums[i];
+    //     if(values.has(complement)) {
+    //         return [values.get(complement), i];
+    //     }
+    //     values.set(nums[i],i);
+    // }
+    // return [];
+    // 2nd Solution, Time Complexity = O(n)
+    let hash = {};
+	
+	for(let i = 0; i < nums.length; i++) {
+		const n = nums[i];
+		if(hash[target - n] !== undefined) {
+			return [hash[target - n], i];
+		}
+		hash[n] = i;
+	}
+	return [];
 };
 
 
