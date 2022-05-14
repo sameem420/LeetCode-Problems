25b13b2d95993370e3ec7ac7a3d03108c786a265
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-15 04:21:44 +0500

    Time: 117 ms (47.27%), Space: 43.5 MB (23.18%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,12 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // Brute-Force Solution, Not Efficient, Time Complexity(n*n)
+    // for(let i=0; i< nums.length; i++) {
+    //     for(let j=i+1; j<nums.length; j++) {
+    //         if(nums[i]+ nums[j] == target)
+    //             return [i,j];
+    //     }
+    // }
+    let values = new Map();
     for(let i=0; i< nums.length; i++) {
-        for(let j=i+1; j<nums.length; j++) {
-            if(nums[i]+ nums[j] == target)
-                return [i,j];
+        let complement = target - nums[i];
+        if(values.has(complement)) {
+            return [values.get(complement), i];
         }
+        values.set(nums[i],i);
     }
+    return [];
 };
 
 
