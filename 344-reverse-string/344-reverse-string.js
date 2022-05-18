2977328a7b9242b7f2a0735cc42c596e86ce67ea
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-18 21:36:59 +0500

    Time: 97 ms (79.50%), Space: 48.9 MB (93.58%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse()
+//     let len = s.length
+//     for (let i = 0; i < s.length / 2; i++) {
+//         // Save current val
+//         let temp = s[i];
+        
+//         // Replace with end of array char
+//         s[i] = s[len - i];
+        
+//         // Replace end of array letter with current val
+//         s[len - i] = temp;
+//     }
+//     return s.shift();
+};
