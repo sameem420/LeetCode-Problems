541239fd56e6a453c6a174f2280350ad5738eb43
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-21 13:58:27 +0500

    Time: 384 ms (10.69%), Space: 50 MB (95.66%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false
+
+  let rev = 0
+  for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
+  return rev === x
+    // let result = 0; xCopy = x;
+    // while(x > 0) {
+    //     rem = x % 10;
+    //     x = ~~(x/10);
+    //     result = result * 10 + rem;
+    // }
+    // return result == xCopy;
+};
