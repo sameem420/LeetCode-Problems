36c23d09f4c533dce29f811903d3319feb04c409
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-26 23:53:18 +0500

    Time: 67 ms (78.11%), Space: 42.1 MB (57.27%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,12 +6,26 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let idx1 = m - 1, idx2 = n - 1, idx3 = m + n - 1;
-  while (idx2 >= 0) {
-    nums1[idx3--] = nums1[idx1] > nums2[idx2]
-      ? nums1[idx1--]
-      : nums2[idx2--];
-  }
+  nums1.splice(m, nums1.length - m);
+    
+    var i = 0;
+    var j = 0;
+    
+    while (j < nums2.length) {
+        if (nums1[i] === undefined || nums1[i] > nums2[j]) {
+            nums1.splice(i, 0, nums2[j]);
+            j++;
+            i++;
+        } else {
+            i++;
+        }
+    }
+    //   let idx1 = m - 1, idx2 = n - 1, idx3 = m + n - 1;
+  // while (idx2 >= 0) {
+  //   nums1[idx3--] = nums1[idx1] > nums2[idx2]
+  //     ? nums1[idx1--]
+  //     : nums2[idx2--];
+  // }
     // m--;n--;
     // let index = nums1.length - 1;
     // while(index >= 0) {
