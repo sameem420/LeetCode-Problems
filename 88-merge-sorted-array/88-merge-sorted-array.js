2163659db5134df283952e6f6577558cc29f9443
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-05-26 23:44:26 +0500

    Time: 71 ms (67.82%), Space: 42.3 MB (44.39%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,23 +6,29 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    m--;n--;
-    let index = nums1.length - 1;
-    while(index >= 0) {
-        if(m < 0) {
-            nums1[index] = nums2[n--];
-        }
-        else if(n < 0) {
-            nums1[index] = nums1[m--];
-        }
-        else {
-            if(nums1[m] > nums2[n]) {
-                nums1[index] = nums1[m--]
-            }
-            else {
-                nums1[index] = nums2[n--]
-            }
-        }
-        index--;
-    }
+    let idx1 = m - 1, idx2 = n - 1, idx3 = m + n - 1;
+  while (idx2 >= 0) {
+    nums1[idx3--] = nums1[idx1] > nums2[idx2]
+      ? nums1[idx1--]
+      : nums2[idx2--];
+  }
+    // m--;n--;
+    // let index = nums1.length - 1;
+    // while(index >= 0) {
+    //     if(m < 0) {
+    //         nums1[index] = nums2[n--];
+    //     }
+    //     else if(n < 0) {
+    //         nums1[index] = nums1[m--];
+    //     }
+    //     else {
+    //         if(nums1[m] > nums2[n]) {
+    //             nums1[index] = nums1[m--]
+    //         }
+    //         else {
+    //             nums1[index] = nums2[n--]
+    //         }
+    //     }
+    //     index--;
+    // }
 };
